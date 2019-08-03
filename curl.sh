f2b1bb5d19b0885d95e6a0254ebfa72b59636906
Eric Iacutone <eric.iacutone@gmail.com>
2019-08-03 13:44:17 -0400

    Replace ping script with curl

---
+++ curl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true
+do
+  curl https://fa-playlist.herokuapp.com
+  sleep 120
+done
--- ping.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# ping.sh 1200 to ping endpoint every 20 minutes
-# Heroku shuts down Hobby dynos after 30 minutes with no usage
-
-ping -i"$1" https://fa-playlist.herokuapp.com
