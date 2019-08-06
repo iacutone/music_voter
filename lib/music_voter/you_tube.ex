ea9151c2521335c8497b91e2f7fa3f584e11b2de
Eric Iacutone <eric.iacutone@gmail.com>
2019-08-05 22:12:17 -0400

    Fix bad commit

--- lib/music_voter/you_tube.ex
+++ lib/music_voter/you_tube.ex
@@ -1,12 +1,11 @@
 defmodule MusicVoter.YouTube do
-    key = System.get_env("YOUTUBE_API_KEY")
   def search(query) do
+    key = System.get_env("YOUTUBE_API_KEY")
     youtube_v3_url = "https://www.googleapis.com/youtube/v3/"
     query = URI.encode(query)
     search = "#{youtube_v3_url}search?part=snippet&maxResults=5&q=#{query}&type=video&key=#{key}"
 
     %{body: body} = HTTPoison.get!(search)
-    IO.inspect body
     decoded = Poison.decode!(body)
     decoded["items"]
   end
